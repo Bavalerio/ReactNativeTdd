0e146a1b6c4fd3cbc714e13662ae98a6fbac575b
Juan Carlos Valerio Arrieta <jcvalerio@appttitude.com>
2019-05-04 13:12:04 -0600

    Agregar configuración inicial de Detox y Jest junto con la primeras pruebas

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Button } from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,12 +18,29 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  state = {
+    helloMessage: '',
+  }
+
+  showHello = () => {
+    this.setState({ helloMessage: 'Hello!!!'})
+  }
+
+  showWorld = () => {
+    this.setState({ helloMessage: 'World!!!'})
+  }
+
   render() {
+    const { helloMessage } = this.state;
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome} testID='welcome'>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <Button testID='hello_button' title="Hello"  onPress={this.showHello} />
+        <Text style={styles.instructions}>{helloMessage}</Text>
+        <Button testID='world_button' title="World"  onPress={this.showWorld} />
       </View>
     );
   }
--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# ReactNativeTdd
+# Proyecto para realizar unit test / e2e testing en React Native
+
+Implementación basica para desarrollar conocimiento de end-to-end testing con Jest y Detox.
+
+# Como instalar
+
+```
+yarn
+```
+
+# Ejecutar pruebas
+
+```
+yarn test
+```
+
+# Ejecutar pruebas E2E
+
+```
+yarn test:e2e
+```
+
+Esto es equivalente a
+
+```
+detox test -c ios.sim.debug
+```
+
+# Comandos importantes
+
+En caso de que el `detox build` falle es recomendable limpiar los caches
+
+```
+watchman watch-del-all; yarn start -- --reset-cache
+```
+
+Check this by typing `xcrun simctl list` in terminal to display all available simulators.
